b7dc8d3fa99b65b10f42adcdc359d3af392baa05
Jmint <jeffreymintah737@gmail.com>
2024-03-26 23:27:09 +0000

    static/js/Detect_NER: used the correct names

--- static/js/Detect_NER.js
+++ static/js/Detect_NER.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
     document.getElementById("api_key").value = cachedApiKey || "";
     document.getElementById("user_input").value = cachedUserInput && "";
-    document.getElementById("answer").value = cachedTargetLang && "";
+    document.getElementById("ner_tags").value = cachedTargetLang && "";
 };
 
 
