c09b9b0aaa0e5bef610784e862c85712a102fb9d
Jmint <jeffreymintah737@gmail.com>
2024-04-01 13:14:51 +0000

    changes

--- static/js/Text_QA.js
+++ static/js/Text_QA.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
     document.getElementById("api_key").value = cachedApiKey || "";
     document.getElementById("user_input").value = cachedUserInput && "";
-    document.getElementById("summary_length").value = cachedQuestion && "";
+    document.getElementById("question").value = cachedQuestion && "";
 };
 
 
