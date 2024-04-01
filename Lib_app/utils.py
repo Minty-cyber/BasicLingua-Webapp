963a4cd32e03a33105433c5130b168043886a2ee
Jmint <jeffreymintah737@gmail.com>
2024-04-01 12:55:07 +0000

    changes

--- Lib_app/utils.py
+++ Lib_app/utils.py
@@ -33,4 +33,6 @@ def TextSummarize(api_key, user_input, summary_length):
 
 def TextQA(api_key, user_input, question):
     client = BasicLingua(api_key=api_key)
+    answer = client.text_summarize(user_input, summary_length)
+    
     
