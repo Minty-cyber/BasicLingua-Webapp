cc771f5a909864b795884cc176bf796b44fb9540
Jmint <jeffreymintah737@gmail.com>
2024-04-01 12:44:10 +0000

    changes

--- static/js/Text_Summarize.js
+++ static/js/Text_Summarize.js
@@ -34,7 +34,7 @@ document.getElementById("summarize-button").addEventListener("click", function()
         success: function(response) {
             document.getElementById("loader").style.display = "none";
             console.log("Response:", response);
-            document.getElementById("summary-result").innerHTML = "<h4>Extraction Result</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.answer + "</p>";
+            document.getElementById("summary-result").innerHTML = "<h4>Summarized Text</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.summary + "</p>";
         },
         error: function(xhr, errmsg, err) {
             document.getElementById("loader").style.display = "none";
