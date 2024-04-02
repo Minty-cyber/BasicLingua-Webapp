37c3370b72a534ab71e7ac703f6fb2220c4e1116
Jmint <jeffreymintah737@gmail.com>
2024-04-02 14:13:59 +0000

    changes

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -131,7 +131,7 @@ def text_intent_view(request):
         if form.is_valid():
             api_key = form.cleaned_data['api_key']
             user_input = form.cleaned_data['user_input']
-            corrected_text = Text_Correction(api_key, user_input) 
+            intent = Text_Correction(api_key, user_input) 
 
             return JsonResponse({'corrected_text': corrected_text})
     else:
