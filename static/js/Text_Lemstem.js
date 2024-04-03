e513dd1db2ede52fd68c0460d636785b8f280d4a
Jmint <jeffreymintah737@gmail.com>
2024-04-03 23:08:28 +0000

    making the right changes

--- static/js/Text_Lemstem.js
+++ static/js/Text_Lemstem.js
@@ -1,53 +1,55 @@
 window.onload = function() {
     var cachedApiKey = localStorage.getItem("api_key");
     var cachedUserInput = localStorage.getItem("user_input");
+    var cachedQuestion = localStorage.getItem("question");
     
 
-    
     document.getElementById("api_key").value = cachedApiKey || "";
     document.getElementById("user_input").value = cachedUserInput && "";
-    
-    
+    document.getElementById("question").value = cachedQuestion && "";
 };
 
-    document.getElementById("process-button").addEventListener("click", function() {
-        var apiKey = document.getElementById("api_key").value.trim();
-        var userInput = document.getElementById("user_input").value.trim();
 
-        if (apiKey === "" || userInput === "") {
-            alert("Please fill in all required fields.");
-            return;
+
+document.getElementById("answer-button").addEventListener("click", function() {
+    var apiKey = document.getElementById("api_key").value.trim();
+    var userInput = document.getElementById("user_input").value.trim();
+    var patterns = document.getElementById("question").value.trim();
+
+    if (apiKey === "" || userInput === "" || question === "") {
+        alert("Please fill in all required fields.");
+        return;
+    }
+
+    document.getElementById("loader").style.display = "block";
+
+    localStorage.setItem("api_key", apiKey);
+    localStorage.setItem("user_input", userInput);
+    localStorage.setItem("question", patterns);
+
+    $.ajax({
+        url: translationUrl,
+        type: "POST",
+        data: $("#translation-form").serialize(),
+        success: function(response) {
+            document.getElementById("loader").style.display = "none";
+            console.log("Response:", response);
+            document.getElementById("answer-result").innerHTML = "<h4>Answer</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.answer + "</p>";
+        },
+        error: function(xhr, errmsg, err) {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("answer-result").innerHTML = "<h4>Error retrieving output</h4><p style='text-align: left;'>" + xhr.status + "</p>";
+            console.log("Error:", xhr.status);
         }
-
-        document.getElementById("loader").style.display = "block";
-
-        localStorage.setItem("api_key", apiKey);
-        localStorage.setItem("user_input", userInput);
-
-        $.ajax({
-            url: translationUrl,
-            type: "POST",
-            data: $("#translation-form").serialize(),
-            success: function(response) {
-                document.getElementById("loader").style.display = "none";
-                console.log("Response:", response);
-                document.getElementById("corrected-result").innerHTML = "<h4>Answer</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.answworkinger + "</p>";
-            },
-            error: function(xhr, errmsg, err) {
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("corrected-result").innerHTML = "<h4>Error retrieving output</h4><p style='text-align: left;'>" + xhr.status + "</p>";
-
-                console.log("Error:", xhr.status + ": " + xhr.responseText);
-            }
-        });
     });
+});
 
+document.getElementById("refresh-button").addEventListener("click", function() {
+    document.getElementById("user_input").value = "";
+    document.getElementById("question").value = "";
+    document.getElementById("answer-result").innerHTML = "";
 
-        document.getElementById("refresh-button").addEventListener("click", function() {
-            document.getElementById("user_input").value = "";
-            document.getElementById("corrected-result").innerHTML = "";
-
-            
-        localStorage.removeItem("user_input");
-        localStorage.removeItem("target_lang");
-    });
+    
+    localStorage.removeItem("user_input");
+    localStorage.removeItem("question");
+});
