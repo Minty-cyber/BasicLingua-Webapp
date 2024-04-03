79d2db9cf9723cb72433590cceaf8897de23ceda
Jmint <jeffreymintah737@gmail.com>
2024-04-03 22:50:54 +0000

    changes

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -146,7 +146,6 @@ def text_lemstem_view(request):
         form = TextLemstemForm(request.POST)
         if form.is_valid():
             api_key = form.cleaned_data['api_key']
-            task_type = form.cleaned_data['task_type']
             user_input = form.cleaned_data['user_input']
             answer = TextLemstem(api_key, user_input, task_type) 
 
