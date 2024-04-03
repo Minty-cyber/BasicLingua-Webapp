bd6067c0698e2465497b57a982498fc5166b1996
Jmint <jeffreymintah737@gmail.com>
2024-04-03 22:47:05 +0000

    changes

--- Lib_app/utils.py
+++ Lib_app/utils.py
@@ -43,5 +43,5 @@ def TextIntent(api_key, user_input):
 
 def TextLemstem(api_key, user_input, task_type):
     client = BasicLingua(api_key=api_key)
-    answer = client.text_lemstem(user_input, )
+    answer = client.text_lemstem(user_input, task_type)
     return answer
