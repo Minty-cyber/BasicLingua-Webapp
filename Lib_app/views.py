2950c8e73cdce0d64e4fd49a06ad2e89d758abc3
Jmint <jeffreymintah737@gmail.com>
2024-04-05 23:08:04 +0000

    changes

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -164,7 +164,6 @@ def text_tokenize_view(request):
         if form.is_valid():
             api_key = form.cleaned_data['api_key']
             user_input = form.cleaned_data['user_input']
-            task_type = form.cleaned_data['task_type']
             answer = TextLemstem(api_key, user_input, task_type) 
 
             return JsonResponse({'answer': answer})
