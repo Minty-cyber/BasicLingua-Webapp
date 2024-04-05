3a30e111414754db91c6724efb722a7188fdc4aa
Jmint <jeffreymintah737@gmail.com>
2024-04-05 22:49:11 +0000

    changes

--- Lib_app/forms.py
+++ Lib_app/forms.py
@@ -43,7 +43,8 @@ class TextLemstemForm(forms.Form):
     user_input = forms.CharField(label='User Input')
     task_type = forms.CharField(label='Task Type')
     
-class TextTokenize(forms. Form)
+class TextTokenize(forms. Form):
+    
 
  
     
