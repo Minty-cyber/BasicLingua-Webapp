7565a6efeffd69e51c2559d8b9378f3981186940
Jmint <jeffreymintah737@gmail.com>
2024-04-06 22:58:58 +0000

    linking the text embedd form to its view

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -176,7 +176,7 @@ def text_embedd_view(request):
     answer = None
 
     if request.method == 'POST':
-        form = TextLemstemForm(request.POST)
+        form = TextEmbeddForm(request.POST)
         if form.is_valid():
             api_key = form.cleaned_data['api_key']
             user_input = form.cleaned_data['user_input']
