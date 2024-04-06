1410ed47e3fbffdf981cf73c8abeae42a6595069
Jmint <jeffreymintah737@gmail.com>
2024-04-06 23:15:20 +0000

    cutting the answers short to ten

--- static/js/Text_Embedd.js
+++ static/js/Text_Embedd.js
@@ -34,7 +34,8 @@ document.getElementById("answer-button").addEventListener("click", function() {
         success: function(response) {
             document.getElementById("loader").style.display = "none";
             console.log("Response:", response);
-            document.getElementById("answer-result").innerHTML = "<h4>Answer</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.answer + "</p>";
+            var truncatedAnswer = response.answer.substring(0, 10)
+            document.getElementById("answer-result").innerHTML = "<h4>Answer</h4><p style='text-align: left; max-width: 100%; overflow-wrap: break-word;'>" + response.answer[:10] + "</p>";
         },
         error: function(xhr, errmsg, err) {
             document.getElementById("loader").style.display = "none";
