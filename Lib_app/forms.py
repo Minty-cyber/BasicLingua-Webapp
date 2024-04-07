975bb0fd0a6f7f8ef53f4323f7a45f11432608f3
Jmint <jeffreymintah737@gmail.com>
2024-04-07 23:29:49 +0000

    changes

--- Lib_app/forms.py
+++ Lib_app/forms.py
@@ -55,7 +55,7 @@ class TextEmbeddForm(forms.Form):
 class TextGenerate(forms.Form):
     api_key = forms.CharField(label='API Key')
     user_input = forms.CharField(label='User Input')
-    ans_length = forms.CharField
+    ans_length = forms.CharField(label=)
     
  
     
