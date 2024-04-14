f83f7144935143b202d15fef0e6a8d87382af6e4
Jmint <jeffreymintah737@gmail.com>
2024-04-14 23:17:28 +0000

    change

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -222,4 +222,4 @@ def text_clean_view(request):
     else:
         form = TextCleanForm()
         
-    return render(request, 'Text_Clean.html', {'form': form, c})
+    return render(request, 'Text_Clean.html', {'form': form, 'answer'})
