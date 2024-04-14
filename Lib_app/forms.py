bf01b7ecd171ea75f30554d05d8352c1cff1e82d
Jmint <jeffreymintah737@gmail.com>
2024-04-14 23:39:31 +0000

    changs

--- Lib_app/forms.py
+++ Lib_app/forms.py
@@ -70,7 +70,7 @@ class TextCleanForm(forms.Form):
 class TextNormalizeForm(forms.Form):
     api_key = forms.CharField(label='API Key')
     user_input = forms.CharField(label='User Input')
-    mode = 
+    mode = forms
 
     
  
