12613772bb95a7b6de98a53e16d3ffb8f34a9cda
Jmint <jeffreymintah737@gmail.com>
2024-04-14 22:50:30 +0000

    changes

--- Lib_app/forms.py
+++ Lib_app/forms.py
@@ -63,7 +63,7 @@ class TextGenerateForm(forms.Form):
 #     num_classes = 
 
 class TextClean(forms.Form):
-    api_key = forms.CharField(label='User')
+    api_key = forms.CharField(label='User Input')
     
  
     
