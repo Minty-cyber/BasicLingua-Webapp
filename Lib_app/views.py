69c899ad88faf98364f742f72a87c1e8a0a78a53
Jmint <jeffreymintah737@gmail.com>
2024-04-14 23:14:10 +0000

    changes

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -213,7 +213,6 @@ def text_clean_view(request):
     if request.method == 'POST':
         form = TextCleanForm(request.POST)
         if form.is_valid():
-                
             api_key = form.cleaned_data['api_key']
             user_input = form.cleaned_data['user_input']
             clean_info = form.cleaned_data['clean_info']
