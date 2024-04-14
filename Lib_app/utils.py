2eabac4a07e6dd3b4de9ea9e957d34214da44b6c
Jmint <jeffreymintah737@gmail.com>
2024-04-14 22:56:54 +0000

    changes

--- Lib_app/utils.py
+++ Lib_app/utils.py
@@ -61,4 +61,4 @@ def TextGenerate(api_key, user_input, ans_length):
     answer = client.text_generate(user_input, ans_length)
     return answer
 
-def TextClean(api_key, user_input,)
+def TextClean(api_key, user_input, clean_info)
