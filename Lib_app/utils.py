80bcd687cc896ef95abed5c99fbde085bc77af7a
Jmint <jeffreymintah737@gmail.com>
2024-04-27 15:03:10 +0000

    changes

--- Lib_app/utils.py
+++ Lib_app/utils.py
@@ -66,4 +66,4 @@ def TextClean(api_key, user_input, clean_info):
     answer = client.text_clean(user_input, clean_info)
     return answer
 
-def TextNormalize(api_key)
+def TextNormalize(api_key, user_input)
