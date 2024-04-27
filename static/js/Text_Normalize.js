96859b1ba4093cf2d0b7d859b0f3060c01259b2e
Jmint <jeffreymintah737@gmail.com>
2024-04-27 16:21:06 +0000

    changes

--- static/js/Text_Normalize.js
+++ static/js/Text_Normalize.js
@@ -25,7 +25,7 @@ document.getElementById("summarize-button").addEventListener("click", function()
 
     localStorage.setItem("api_key", apiKey);
     localStorage.setItem("user_input", userInput);
-    localStorage.setItem("mode", patterns);
+    localStorage.setItem("mode", mode);
 
     $.ajax({
         url: translationUrl,
