b3fc0ac44513672ddd3e237946303398cdaff840
Jmint <jeffreymintah737@gmail.com>
2024-05-07 10:00:33 +0000

    changes

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -245,7 +245,7 @@ def text_tokenize_view(request):
     srl_result = None
 
     if request.method == 'POST':
-        form = TextTokenizeForm(request.POST)
+        form = TextSRLForm(request.POST)
         if form.is_valid():
             api_key = form.cleaned_data['api_key']
             user_input = form.cleaned_data['user_input']
