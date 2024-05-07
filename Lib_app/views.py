f72043e74b98a5f52ae2d23624817bd64303c563
Jmint <jeffreymintah737@gmail.com>
2024-05-07 10:01:01 +0000

    change

--- Lib_app/views.py
+++ Lib_app/views.py
@@ -251,7 +251,7 @@ def text_tokenize_view(request):
             user_input = form.cleaned_data['user_input']
             answer = TextSRL(api_key, user_input) 
 
-            return JsonResponse({'answer': answer})
+            return JsonResponse({'srl_result': answer})
     else:
         form = TextTokenizeForm()
 
