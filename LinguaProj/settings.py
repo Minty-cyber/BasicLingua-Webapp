08d1ff0351ee701ad413aa70ce2cdc4414c21596
Jmint <jeffreymintah737@gmail.com>
2024-07-06 09:24:39 +0000

    changes

--- LinguaProj/settings.py
+++ LinguaProj/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
