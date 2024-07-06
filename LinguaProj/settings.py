5ef4ff36ffe40a1139f7f7f6b9dd96f922c1cef0
Jmint <jeffreymintah737@gmail.com>
2024-07-06 09:09:57 +0000

    changes

--- LinguaProj/settings.py
+++ LinguaProj/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'assets'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
